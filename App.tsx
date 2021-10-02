c63def68af3dc11e66db203250a17b4a316ef9c9
Jair Diaz <jairdiaz@blacksip.com>
2021-10-01 22:30:25 -0500

    fist commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [image, setImage] = useState('https://picsum.photos/400');
+  const getImage = () => {
+    setImage(
+      `https://picsum.photos/400?v=${(Math.random() + 1)
+        .toString(36)
+        .substring(7)}`
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Hello world Jair</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          getImage();
+        }}
+      >
+        <Text style={styles.textButton}>Click me</Text>
+      </TouchableOpacity>
+      <Image
+        style={styles.image}
+        source={{
+          uri: image,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +40,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'red',
+    marginTop: 20,
+    padding: 5,
+    borderRadius: 50,
+  },
+  textButton: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  image: {
+    borderColor: 'black',
+    borderWidth: 2,
+    marginTop: 20,
+    height: 200,
+    width: 200,
+    borderRadius: 200,
   },
 });
---
+++ readme.md
@@ -0,0 +1,3 @@
+1. instalar expo `npm install -g expo-cli`
+2. ejecutar `expo init`
+3. compilara `yarn start` o `node start`
