57d64d42257f8bc69184cdd246c6435df683f87a
Jair Diaz <jairdiaz@blacksip.com>
2021-10-19 20:05:06 -0500

    feat: se carga la configuracion de typescript

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
   Image,
   TouchableOpacity,
   Platform,
+  ScrollView,
 } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Sharing from 'expo-sharing';
@@ -16,16 +17,17 @@ const images = {
 
 export default function App() {
   const [image, setImage] = useState('https://picsum.photos/400');
-  const [permisos, setPermisos] = useState({ camara: false });
+  const [permisos, setPermisos] = useState({ files: false });
   const [imageBrowse, setImageBrowse] = useState(images.empty);
   const getPermisos = async () => {
     if (Platform.OS !== 'web') {
-      const {
-        status,
-      } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
       if (status === 'granted') {
-        setPermisos({ ...permisos, camara: true });
+        setPermisos({ ...permisos, files: true });
       }
+    } else {
+      setPermisos({ ...permisos, files: true });
     }
   };
   useMemo(async () => {
@@ -39,13 +41,11 @@ export default function App() {
     );
   };
   const pickImageLocal = async () => {
-    if (permisos.camara !== true) {
+    if (permisos.files !== true) {
       await getPermisos();
     } else {
       let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 4],
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
         quality: 1,
       });
       if (!result.cancelled) {
@@ -54,45 +54,49 @@ export default function App() {
     }
   };
   const shareImage = async () => {
-    if (!(await Sharing.isAvailableAsync())) {
-      alert(`La opcion de compartir no esta disponible en este dispositivo`);
+    if (Platform.OS === 'web' || !(await Sharing.isAvailableAsync())) {
+      alert(`No se pueden compartir imagenes desde esta plataforma`);
       return;
     }
-    await Sharing.shareAsync(imageBrowse?.uri, {});
+    await Sharing.shareAsync(imageBrowse?.uri);
   };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>App images</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          getImage();
-        }}
-      >
-        <Text style={styles.textButton}>Change image</Text>
-      </TouchableOpacity>
-      <Image
-        style={styles.image}
-        source={{
-          uri: image,
-        }}
-      />
-      <TouchableOpacity
-        onPress={() => {
-          pickImageLocal();
-        }}
-      >
-        <Image style={styles.image2} source={imageBrowse} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          shareImage();
-        }}
-      >
-        <Text style={styles.textButton}>Share image</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <Text style={styles.title}>App images</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            getImage();
+          }}
+        >
+          <Text style={styles.textButton}>Change image</Text>
+        </TouchableOpacity>
+        <Image
+          style={styles.image}
+          source={{
+            uri: image,
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            pickImageLocal();
+          }}
+        >
+          <Image style={styles.image2} source={imageBrowse} />
+        </TouchableOpacity>
+        {imageBrowse.uri && (
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              shareImage();
+            }}
+          >
+            <Text style={styles.textButton}>Share image</Text>
+          </TouchableOpacity>
+        )}
+        <StatusBar style="auto" />
+      </ScrollView>
     </View>
   );
 }
